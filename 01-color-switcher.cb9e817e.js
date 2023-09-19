c1fbcc89ccbbde1f3c10cdb65471eb202ff25ae6
kglanos <130902847+kglanos@users.noreply.github.com>
2023-09-19 19:43:41 +0000

    Deploying to gh-pages from @ kglanos/goit-js-hw-09@7391167ec350047a1651688308183ac9f36eef52 🚀

---
+++ 01-color-switcher.cb9e817e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.body;let a=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;d.style.background=e}),1e3)})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.cb9e817e.js.map
