7391167ec350047a1651688308183ac9f36eef52
kglanos <130902847+kglanos@users.noreply.github.com>
2023-09-19 21:43:14 +0200

    Update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const body = document.body;
+let IntervalId = null;
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    IntervalId = setInterval(() => {
+        const randomColor  = getRandomHexColor();
+        body.style.background = randomColor;
+    }, 1000);
+});
+
+stopButton.addEventListener('click', () =>{
+    clearInterval(IntervalId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+});
